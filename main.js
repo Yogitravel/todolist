fe769081363ee3660fe412c8c558f468409f011a
yogitravel <64913300+thuongbien@users.noreply.github.com>
2020-05-20 09:50:19 +0700

    lancuoi

--- main.js
+++ main.js
@@ -14,19 +14,31 @@ let addItem = () => {
 
 //su dung index de chon ngau nhien
 let showList = (list) => {
-    let message = list.map((item, index) => `<li> 
 
-    <input type = "checkbox" >
-    <a onclick="doneTask(${index})" href="#">Done</a> 
-    ${item.contents}'
+    let message = list.map((item, index) => {
+        if (item.isDone) {
+            return `<li>  ${item.contents}'
+        <button onclick="removeItem(${index})">Delete</button>
+        <a href="#" onclick ="toggle(${index})"> UNDONE </a> </li>`.strike()
+
+        } else {
+            return `</li >  ${item.contents}'
+            
     <button onclick="removeItem(${index})">Delete</button>
-    
-    </li>`).join('')
+    <a href="#" onclick ="toggle(${index})"> DONE </a> </li>`
+
+        }
+    }).join('')
 
     document.getElementById("resultArea").innerHTML = message
 }
 
+let toggle = (index) => {
+    console.log("running")
+    itemList[index].isDone = !itemList[index].isDone
+    showList(itemList)
 
+}
 
 document.getElementById("btn-add").addEventListener("click", addItem)
 
@@ -35,18 +47,19 @@ function removeItem(index) {
     itemList.splice(index, 1);
     showList(itemList)
 }
+
 ///tao linh trong js, ben canh chu delete
 //neu bam vao link thi gach ngang cai dong do
 // va chuyen thuoc tinh isDone = true
 
-function doneTask(index) {
-    if (itemList[index].isDone == true) {
-        itemList[index].isDone = false
-    } else {
-        itemList[index].isDone = true
-    }
-    console.log(itemList)
-}
+// function doneTask(index) {
+//     if (itemList[index].isDone == true) {
+//         itemList[index].isDone = false
+//     } else {
+//         itemList[index].isDone = true
+//     }
+//     console.log(itemList)
+// }
 
 
 //add "Done" field
